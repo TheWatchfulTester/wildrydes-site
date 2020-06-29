f949b94a9b8810cde6e3902f0351d9578bf08764
WatchfulTester <Watchfultester@gmail.com>
2020-06-29 15:22:06 +1200

    region for user pools

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_ClRRCsu7Y', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5heg5jtcg0brc6cf7m2o6iakgd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
